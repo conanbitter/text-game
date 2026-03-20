3acd289c4cf9d414c3be4279f462b8ed7d396aa0
Conan Bitter <conan.bitter@gmail.com>
2026-03-20 19:17:10 +0300

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL.h>
+
+SDL_Window* window;
+
+int main() {
+
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("Forth", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
+        SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            switch (e.type)
+            {
+            case SDL_EVENT_QUIT:
+                quit = true;
+                break;
+
+            case SDL_EVENT_KEY_DOWN:
+                switch (e.key.key)
+                {
+                case SDLK_ESCAPE:
+                    quit = true;
+                    break;
+
+                default:
+                    break;
+                }
+
+            default:
+                break;
+            }
+        }
+
+        SDL_Delay(5);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
