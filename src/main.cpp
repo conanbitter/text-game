a5589bc7d00ebcccb0e2dab978452c4c940713b2
Conan Bitter <conan.bitter@gmail.com>
2026-04-16 08:42:41 +0300

    Implement proper resource loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <SDL3/SDL_main.h>
 #include "app.hpp"
 #include "textures.hpp"
+#include <SDL3/SDL.h>
 
 class MyApp :public App {
 public:
     void load() override {
         tex = res.get<Texture>("assets/bitmap.png");
+        int w, h;
+        tex->getSize(w, h);
+        SDL_Log("Tex1 size: %d x %d", w, h);
+        PTexture tex2 = res.get<Texture>("assets/bitmap.png");
+        tex2->getSize(w, h);
+        SDL_Log("Tex2 size: %d x %d", w, h);
     }
 
     void draw() override {
--- src/resources.cpp
+++ src/resources.cpp
@@ -1,17 +1,30 @@
 #include "resources.hpp"
 #include "textures.hpp"
+#include "SDL3/SDL.h"
 
 template <typename T>
     requires std::derived_from<T, Resource>
 std::shared_ptr<T> ResourceManager::get(const std::string& filename) {
 
-    /*auto it = resources.find(key);
+    auto it = resources.find(filename);
     if (it != resources.end()) {
-        return static_cast<std::shared_ptr<T>>(it->second.get());
-    }*/
+        if (std::shared_ptr<Resource> entry = it->second.lock()) {
+            std::shared_ptr<T> result = std::dynamic_pointer_cast<T>(entry);
+            if (result->isValid()) {
+#ifdef DEBUG        
+                SDL_Log("Getting resource \"%s\" from cache", filename.c_str());
+#endif
+                return result;
+            }
+        }
+    }
 
+#ifdef DEBUG        
+    SDL_Log("Loading resource \"%s\"", filename.c_str());
+#endif
     std::shared_ptr<T> result = std::make_shared<T>();
     result->load(filename);
+    resources[filename] = result;
     return result;
 }
 
