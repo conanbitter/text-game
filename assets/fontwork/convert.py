295573f876a1e549c311d301fe99ff06b0c97a2b
Conan Bitter <conan.bitter@gmail.com>
2026-04-17 17:25:34 +0300

    New font format

--- assets/fontwork/convert.py
+++ assets/fontwork/convert.py
@@ -1,26 +1,26 @@
 import json
 import struct
 
-raw_data = None
+font_data = None
 kerning_data = None
 
 with open("font.json", 'r') as inf:
-    raw_data = json.load(inf)
+    font_data = json.load(inf)
 
 with open("kerning.json", 'r') as inf:
     kerning_data = json.load(inf)
 
 outf = open("../font.fnt", 'wb')
 
-em_size = raw_data['metrics']['emSize'] * raw_data['atlas']['size']
+em_size = font_data['metrics']['emSize'] * font_data['atlas']['size']
 
-image_width = raw_data['atlas']['width']
-image_height = raw_data['atlas']['height']
+image_width = font_data['atlas']['width']
+image_height = font_data['atlas']['height']
 space_advance = 0.0
 glyph_count = 0
 glyphs = set()
 
-for glyph_data in raw_data['glyphs']:
+for glyph_data in font_data['glyphs']:
     if glyph_data['unicode'] == 32:
         space_advance = glyph_data['advance'] * em_size
     if 'atlasBounds' in glyph_data:
@@ -36,12 +36,36 @@ for kd in kerning_data['pairs']:
     if (gleft in glyphs) and (gright in glyphs):
         kerning.append({'left': gleft, 'right': gright, 'kerning': kern / kern_emsize})
 
-sdf_range = raw_data['atlas']['distanceRange']
+sdf_range = font_data['atlas']['distanceRange']
 glyph_count = glyph_count
+font_size = font_data['atlas']['size']
+line_height = font_data['metrics']['lineHeight'] * font_size
 
-outf.write(struct.pack('ffLL', sdf_range, space_advance, glyph_count, len(kerning)))
+# Writing file
 
-for glyph_data in raw_data['glyphs']:
+outf.write(b"FONT")  # Magic
+outf.write(struct.pack('B', 1))  # Version
+
+# Header
+
+outf.write(b"HEAD")
+outf.write(struct.pack('I', 4 * 4))
+outf.write(struct.pack('ffff', font_size, line_height, sdf_range, space_advance))
+
+# Font texture
+
+TEXTURE_FILENAME = b"font.png"
+
+outf.write(b"IMAG")
+outf.write(struct.pack('I', len(TEXTURE_FILENAME)))
+outf.write(TEXTURE_FILENAME)
+
+# Glyphs
+
+outf.write(b"CHAR")
+outf.write(struct.pack('I', glyph_count * 8 * 4))
+
+for glyph_data in font_data['glyphs']:
     glyph_code = glyph_data['unicode']
     if 'atlasBounds' not in glyph_data:
         continue
@@ -50,10 +74,8 @@ for glyph_data in raw_data['glyphs']:
     glyph_rect_w = glyph_data['atlasBounds']['right'] - glyph_data['atlasBounds']['left']
     glyph_rect_h = glyph_data['atlasBounds']['top'] - glyph_data['atlasBounds']['bottom']
 
-    # glyph_width = glyph_data['planeBounds']['right'] - glyph_data['planeBounds']['left']
-    # glyph_height = glyph_data['planeBounds']['top'] - glyph_data['planeBounds']['bottom']
-    glyph_originx = -glyph_data['planeBounds']['left'] * em_size  # / glyph_width * glyph_rect_w
-    glyph_originy = glyph_data['planeBounds']['top'] * em_size  # / glyph_height * glyph_rect_h
+    glyph_originx = -glyph_data['planeBounds']['left'] * em_size
+    glyph_originy = glyph_data['planeBounds']['top'] * em_size
 
     glyph_advance = glyph_data['advance'] * em_size
 
@@ -62,8 +84,14 @@ for glyph_data in raw_data['glyphs']:
     # print(f"Origin x:{glyph_originx} y:{glyph_originy}")
     # print(f"Advance {glyph_advance}")
     # print(glyph_data)
-    outf.write(struct.pack('Lfffffff', glyph_code, glyph_rect_x, glyph_rect_y, glyph_rect_w, glyph_rect_h, glyph_originx, glyph_originy, glyph_advance))
+    outf.write(struct.pack('Ifffffff', glyph_code, glyph_rect_x, glyph_rect_y, glyph_rect_w, glyph_rect_h, glyph_originx, glyph_originy, glyph_advance))
+
+# Kerning
+
+outf.write(b"KERN")
+outf.write(struct.pack('I', len(kerning) * 3 * 4))
 
 for k in kerning:
     outf.write(struct.pack('LLf', k['left'], k['right'], k['kerning']))
+
 outf.close()
